a9cbaef141aab3c3d71c0d4402a1f902651c748a
Salvatore Paone <salvatorepaone@MacBookPro.station>
2026-06-26 16:37:15 +0200

    fix: aggiungi gate Filament per produzione

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,24 +3,23 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Gate;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
         if (config('app.env') === 'production') {
             \Illuminate\Support\Facades\URL::forceScheme('https');
         }
+
+        Gate::define('viewFilament', function ($user) {
+            return true;
+        });
     }
 }
